7292839aa83cc4684bda7d3f06e1f4f85acb4bc3
mohreh <mrezahassani1378@gmail.com>
2026-04-21 21:26:37 +0330

    boilerplate for server.rs

--- build.rs
+++ build.rs
@@ -1,7 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_prost_build::configure()
         .build_server(true)
-        .out_dir("src/")
         .compile_protos(&["proto/zkp_auth.proto"], &["proto/"])?;
     Ok(())
 }
--- src/server.rs
+++ src/server.rs
@@ -1,9 +1,58 @@
 pub mod zkp_auth {
-    include!("./zkp_auth.rs");
+    // مسیر به فایل تولید شده توسط tonic-build
+    include!(concat!(env!("OUT_DIR"), "/zkp_auth.rs"));
 }
 
-use zkp_auth::auth_service_server;
+use tonic::{Request, Response, Status};
+use zkp_auth::auth_service_server::{AuthService, AuthServiceServer};
 
-fn main() {
-    println!("Hi, I am the server");
+use crate::zkp_auth::{
+    CreateAuthenticationChallengeRequest, CreateAuthenticationChallengeResponse, RegisterRequest,
+    RegisterResponse, VerifyAuthenticationRequest, VerifyAuthenticationResponse,
+};
+
+#[derive(Debug, Default)]
+struct Auth {}
+
+#[tonic::async_trait]
+impl AuthService for Auth {
+    async fn register(
+        &self,
+        req: Request<RegisterRequest>,
+    ) -> Result<Response<RegisterResponse>, Status> {
+        todo!()
+    }
+
+    async fn create_authentication_challenge(
+        &self,
+        req: Request<CreateAuthenticationChallengeRequest>,
+    ) -> Result<Response<CreateAuthenticationChallengeResponse>, Status> {
+        todo!()
+    }
+
+    async fn verify_authentication(
+        &self,
+        req: Request<VerifyAuthenticationRequest>,
+    ) -> Result<Response<VerifyAuthenticationResponse>, Status> {
+        todo!()
+    }
+}
+
+use std::env;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+
+    let port = env::var("PORT").unwrap_or_else(|_| "50051".to_string());
+    let addr = format!("0.0.0.0:{}", port).parse()?;
+    log::info!("🚀 Starting ZKP Auth gRPC server on {}", addr);
+
+    let auth = Auth::default();
+    tonic::transport::Server::builder()
+        .add_service(AuthServiceServer::new(auth))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
